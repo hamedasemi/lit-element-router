2ab52991f0783f9ef65257bbcf5b879f5061114a
= <mszado15@gmail.com>
2019-04-19 13:00:40 -0500

    Added styles for show it

--- router/router-link.js
+++ router/router-link.js
@@ -1,33 +1,32 @@
 import { LitElement, html } from 'lit-element'
 
 export class RouterLink extends LitElement {
+
     constructor() {
         super()
         this.addEventListener('click', this.clickHandler.bind(this))
     }
+
+    createRenderRoot () {
+        return this;
+    }
+
     clickHandler(event) {
         event.preventDefault();
         window.history.pushState({}, null, event.target.href + window.location.search)
         window.dispatchEvent(new CustomEvent('route'))
     }
+
     static get properties() {
         return {
             href: { type: String }
         }
     }
+
     render() {
         return html`
-            <style>
-                ::slotted(*) {
-                    pointer-events: none;
-                }
-                a {
-                    all: unset;
-                    display: contents;
-                }
-            </style>
             <a href='${this.href}'>
-                <slot></slot>
+                ${this.textContent}
             </a>
         `
     }
