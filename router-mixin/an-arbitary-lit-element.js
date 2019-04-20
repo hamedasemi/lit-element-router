1130b9f3382170274b1129b2a3153a2d2319f5eb
= <mszado15@gmail.com>
2019-04-19 21:26:56 -0500

    Implemeting extensible elements in Anchor Element for route-link, modern and easy

--- router-mixin/an-arbitary-lit-element.js
+++ router-mixin/an-arbitary-lit-element.js
@@ -1,25 +1,16 @@
-import { LitElement, html } from 'lit-element'
 import { routerLinkMixin } from '../lit-element-router'
 
-export class AnArbitaryLitElement extends routerLinkMixin(LitElement) {
+export class RouterLink extends routerLinkMixin(HTMLAnchorElement) {
     constructor() {
         super()
-        this.href = ''
+        this.href = this.href;
+        this.addEventListener('click', e => this.linkClick(e));
     }
-    static get properties() {
-        return {
-            href: { type: String }
-        }
-    }
-    render() {
-        return html`
-            <a href='${this.href}' @click='${this.linkClick}'><slot></slot></a>
-        `
-    }
+
     linkClick(event) {
         event.preventDefault();
         this.navigate(this.href);
     }
 }
 
-customElements.define('an-arbitary-lit-element', AnArbitaryLitElement)
+customElements.define('route-link', RouterLink, {extends: 'a'})
