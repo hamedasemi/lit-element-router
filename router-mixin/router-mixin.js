63228d278ee5a3518ae1d02189bd32d340d6d302
hamedasemi <h-@live.se>
2019-11-15 22:01:29 +0100

    Update route to activeRoute Clean code Fix not found bug Remove router prefix Rename selected to active Remove not used CSS

--- router-mixin/router-mixin.js
+++ router-mixin/router-mixin.js
@@ -28,54 +28,50 @@ export let routerMixin = (superclass) => class extends superclass {
         const querystring = decodeURI(window.location.search);
 
         let notFoundRoute = routes.filter(route => route.pattern === '*')[0];
+        let activeRoute = routes.filter(route => route.pattern !== '*' && testRoute(uri, route.pattern))[0];
 
-        routes = routes.filter(route => route.pattern !== '*' && testRoute(uri, route.pattern));
-
-        if (routes.length) {
-            let route = routes[0];
-            route.params = parseParams(route.pattern, uri);
-            route.query = parseQuery(querystring);
-
-            if (route.guard && typeof route.guard === 'function') {
-
+        if (activeRoute) {
+            activeRoute.params = parseParams(activeRoute.pattern, uri);
+            activeRoute.query = parseQuery(querystring);
+            if (activeRoute.guard && typeof activeRoute.guard === 'function') {
                 this.canceled = false;
-                Promise.resolve(route.guard())
+                Promise.resolve(activeRoute.guard())
                     .then((allowed) => {
                         if (!this.canceled) {
                             if (allowed) {
-                                route.callback && route.callback(route.name, route.params, route.query, route.data);
-                                callback(route.name, route.params, route.query, route.data);
+                                activeRoute.callback && activeRoute.callback(activeRoute.name, activeRoute.params, activeRoute.query, activeRoute.data);
+                                callback(activeRoute.name, activeRoute.params, activeRoute.query, activeRoute.data);
                             } else {
-                                route.callback && route.callback('not-authorized', route.params, route.query, route.data);
-                                callback('not-authorized', route.params, route.query, route.data);
+                                activeRoute.callback && activeRoute.callback('not-authorized', activeRoute.params, activeRoute.query, activeRoute.data);
+                                callback('not-authorized', activeRoute.params, activeRoute.query, activeRoute.data);
                             }
                         }
                     })
             } else {
-                route.callback && route.callback(route.name, route.params, route.query, route.data)
-                callback(route.name, route.params, route.query, route.data);
+                activeRoute.callback && activeRoute.callback(activeRoute.name, activeRoute.params, activeRoute.query, activeRoute.data)
+                callback(activeRoute.name, activeRoute.params, activeRoute.query, activeRoute.data);
             }
         } else if (notFoundRoute) {
-            notFoundRoute.callback && notFoundRoute.callback(notFoundRoute.name, {}, parseQuery(querystring), notFoundRoute.data)
-            callback(notFoundRoute.name, {}, parseQuery(querystring), notFoundRoute.data);
+            notFoundRoute.callback && notFoundRoute.callback(notFoundRoute.name, {}, {}, notFoundRoute.data)
+            callback(notFoundRoute.name, {}, {}, notFoundRoute.data);
         } else {
-            callback('not-found', {}, parseQuery(querystring), notFoundRoute.data);
+            callback('not-found', {}, {}, {});
         }
     }
 };
 
-export let routerLinkMixin = (superclass) => class extends superclass {
+export let linkMixin = (superclass) => class extends superclass {
     navigate(href) {
         window.history.pushState({}, null, href);
         window.dispatchEvent(new CustomEvent('route'));
     }
 };
 
-export let routerOutletMixin = (superclass) => class extends superclass {
+export let outletMixin = (superclass) => class extends superclass {
 
     static get properties() {
         return {
-            currentRoute: { type: String, reflect: true, attribute: 'current-route' }
+            activeRoute: { type: String, reflect: true, attribute: 'active-route' }
         }
     }
 
@@ -83,10 +79,7 @@ export let routerOutletMixin = (superclass) => class extends superclass {
         super();
 
         let newStyleSheet = new CSSStyleSheet;
-        newStyleSheet.replaceSync( `
-            ::slotted([route]:not([selected])) { display: none; }
-            [route]:not([selected]) { display: none; }
-        `);
+        newStyleSheet.replaceSync( `::slotted([route]:not([active])) { display: none; }`);
 
         this.shadowRoot.adoptedStyleSheets = [...this.shadowRoot.adoptedStyleSheets, newStyleSheet];
     }
@@ -94,22 +87,22 @@ export let routerOutletMixin = (superclass) => class extends superclass {
     attributeChangedCallback(...args) {
         super.attributeChangedCallback(...args);
 
-        args[0] === 'current-route' && this.routerOutlet();
+        args[0] === 'active-route' && this.outlet();
     }
 
     connectedCallback(...args) {
         super.connectedCallback(...args);
         
-        this.routerOutlet();
+        this.outlet();
     }
 
-    routerOutlet() {
-        Array.from(this.querySelectorAll(`[route]`)).map((selected) => {
-            selected.removeAttribute('selected');
+    outlet() {
+        Array.from(this.querySelectorAll(`[route]`)).map((active) => {
+            active.removeAttribute('active');
         });
-        if (this.currentRoute) {
-            Array.from(this.querySelectorAll(`[route~=${this.currentRoute}]`)).map((selected) => {
-                selected.setAttribute('selected', true);
+        if (this.activeRoute) {
+            Array.from(this.querySelectorAll(`[route~=${this.activeRoute}]`)).map((active) => {
+                active.setAttribute('active', true);
             });
         }
     }
