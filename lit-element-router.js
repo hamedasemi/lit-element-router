4e6d3e7f4b81209ab922bed2831912e7d209850a
hamedasemi <h-@live.se>
2019-11-15 19:14:16 +0100

    Semicolons added

--- lit-element-router.js
+++ lit-element-router.js
@@ -1,7 +1,7 @@
-import { router } from './router/router'
-import { routerMixin, routerLinkMixin, routerOutletMixin } from './router-mixin/router-mixin'
+import { router } from './router/router';
+import { routerMixin, routerLinkMixin, routerOutletMixin } from './router-mixin/router-mixin';
 
-import { RouterLink } from './router/router-link'
-import { RouterSlot } from './router/router-slot'
+import { RouterLink } from './router/router-link';
+import { RouterSlot } from './router/router-slot';
 
-export { router, RouterLink, RouterSlot, routerMixin, routerLinkMixin, routerOutletMixin }
+export { router, RouterLink, RouterSlot, routerMixin, routerLinkMixin, routerOutletMixin };
