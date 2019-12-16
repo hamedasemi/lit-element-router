d57f439be256a128aa0e6a543e81f8d2bdba1bc2
hamedasemi <h-@live.se>
2019-12-16 20:33:42 +0100

    Rremove not used comments

--- utility/router-utility.js
+++ utility/router-utility.js
@@ -1,7 +1,3 @@
-/**
- * 
- * @param {String} str - The uri that has extra slashes
- */
 export function stripExtraTrailingSlash(str) {
     while (str.length !== 1 && str.substr(-1) === '/') {
         str = str.substr(0, str.length - 1);
@@ -9,20 +5,10 @@ export function stripExtraTrailingSlash(str) {
     return str;
 }
 
-/**
- * 
- * @param {String} querystring - The author of the book.
- */
 export function parseQuery(querystring) {
     return querystring ? JSON.parse('{"' + querystring.substring(1).replace(/&/g, '","').replace(/=/g, '":"') + '"}') : {}
 }
 
-/**
- * Desc
- * @param {String} pattern - The pattern
- * @param {String} uri - The current uri
- * @return {Object} - The uri params object
- */
 export function parseParams(pattern, uri) {
     let params = {}
 
@@ -37,10 +23,6 @@ export function parseParams(pattern, uri) {
     return params
 }
 
-/**
- * À-ÖØ-öø-ÿ
- * @param {*} pattern 
- */
 export function patternToRegExp(pattern) {
     if (pattern) {
         return new RegExp('^(|/)' + pattern.replace(/:[^\s/]+/g, '([\\w\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff-]+)') + '(|/)$');
