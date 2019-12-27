805b6107f55cbfa7e86b93a46ec51990224c5593
hamedasemi <h-@live.se>
2019-12-27 21:15:37 +0100

    Support decorators

--- lit-element-router.d.ts
+++ lit-element-router.d.ts
@@ -1,4 +1,4 @@
-import { LitElement } from 'lit-element';
+// import { LitElement } from 'lit-element';
 
 interface Unauthenticated {
     name: string;
@@ -29,20 +29,86 @@ interface Route {
 
 interface Routes extends Array<Route> { }
 
-export const routerMixin = (superclass: typeof LitElement) => class RouterMixin extends superclass {
-    static routes: Routes;
-    protected router(route?: string, params?: object, query?: object, data?: object): void;
-    private routed(name?: string, params?: object, query?: object, data?: object, callback?: (route?: string, params?: object, query?: object, data?: object) => void, localCallback?: (route?: string, params?: object, query?: object, data?: object) => void): void;
-    private routing(routes: Routes, callback: (route: string, params: object, query: object, data: object) => void): void;
+export function router<TBase extends typeof HTMLElement>(base: TBase) {
+    return class Router extends base<TBase> {
+        static routes: Routes;
+        protected router(route?: string, params?: object, query?: object, data?: object): void;
+        private routed(name?: string, params?: object, query?: object, data?: object, callback?: (route?: string, params?: object, query?: object, data?: object) => void, localCallback?: (route?: string, params?: object, query?: object, data?: object) => void): void;
+        private routing(routes: Routes, callback: (route: string, params: object, query: object, data: object) => void): void;
+    }
 }
 
-export const navigateMixin = (superclass: typeof LitElement) => class NavigateMixin extends superclass {
-    protected navigate(href: string): void;
+// export function router() {
+//     return function <TBase extends typeof HTMLElement>(base: TBase) {
+//         return class Router extends base {
+//             static routes: Routes;
+//             protected router(route?: string, params?: object, query?: object, data?: object): void;
+//             private routed(name?: string, params?: object, query?: object, data?: object, callback?: (route?: string, params?: object, query?: object, data?: object) => void, localCallback?: (route?: string, params?: object, query?: object, data?: object) => void): void;
+//             private routing(routes: Routes, callback: (route: string, params: object, query: object, data: object) => void): void;
+//         };
+//     }
+// }
+
+export function navigator<TBase extends typeof HTMLElement>(base: TBase) {
+    return class Navigator extends base<TBase> {
+        protected navigate(href: string): void;
+    }
 }
 
-export const outletMixin = (superclass: typeof LitElement) => class OutletMixin extends superclass {
-    private outlet(): void;
+// type Constructor<T = HTMLElement> = new (...args: any[]) => T;
+// export declare function navigator<TBase extends typeof HTMLElement>(Base: TBase) {
+//     return class navigator extends Base {
+//         new(...args: any[]): {
+//             protected navigate(href: string): void;
+//         };
+//         protected navigate(href: string): void;    }
+// }
+
+
+
+// import { LitElement } from "lit-element";
+// declare type Constructor = new (...args: any[]) => HTMLElement;
+// interface BeforeRenderMixin {
+//     new(...args: any[]): {
+//                      navigate(href: string): void;
+//                 };
+//     protected navigate(href: string): void;
+// }
+// declare type ReturnConstructor = new (...args: any[]) => HTMLElement & BeforeRenderMixin;
+// export function navigator<B extends Constructor>(Base: B): B & ReturnConstructor;
+
+
+
+// class Navigator {
+//     new(...args: any[]): {
+//         navigate(href: string): void;
+//     };
+//     protected navigate(href: string): void;
+// }
+
+// export declare function navigator<T extends typeof HTMLElement>(base: T): T & Navigator
+
+
+export function outlet<TBase extends typeof HTMLElement>(base: TBase) {
+    return class Outlet extends base<TBase> {
+        private outlet(): void;
+    }
 }
+
+// export const router = (superclass: typeof LitElement) => class Router extends superclass {
+//     static routes: Routes;
+//     protected router(route?: string, params?: object, query?: object, data?: object): void;
+//     private routed(name?: string, params?: object, query?: object, data?: object, callback?: (route?: string, params?: object, query?: object, data?: object) => void, localCallback?: (route?: string, params?: object, query?: object, data?: object) => void): void;
+//     private routing(routes: Routes, callback: (route: string, params: object, query: object, data: object) => void): void;
+// }
+
+// export const navigate = (superclass: typeof LitElement) => class Navigate extends superclass {
+//     protected navigate(href: string): void;
+// }
+
+// export const outlet = (superclass: typeof LitElement) => class Outlet extends superclass {
+//     private outlet(): void;
+// }
 
 
 // type Constructor<T = HTMLElement> = new (...args: any[]) => T;
--- lit-element-router.js
+++ lit-element-router.js
@@ -1,144 +1,150 @@
 import { parseParams, parseQuery, testRoute } from './utility/router-utility';
 
-export let routerMixin = (superclass) => class extends superclass {
-    static get properties() {
-        return {
-            route: { type: String, reflect: true, attribute: 'route' },
-            canceled: { type: Boolean }
-        };
-    }
+export function router(base) {
+    return class extends base {
+        static get properties() {
+            return {
+                route: { type: String, reflect: true, attribute: 'route' },
+                canceled: { type: Boolean }
+            };
+        }
 
-    constructor(...args) {
-        super(...args);
+        constructor(...args) {
+            super(...args);
 
-        this.route = '';
-        this.canceled = false;
-    }
+            this.route = '';
+            this.canceled = false;
+        }
 
-    connectedCallback(...args) {
-        super.connectedCallback(...args);
+        connectedCallback(...args) {
+            super.connectedCallback(...args);
 
-        // @ts-ignore
-        this.routing(this.constructor.routes, (...args) => this.router(...args));
-        window.addEventListener('route', () => {
             // @ts-ignore
             this.routing(this.constructor.routes, (...args) => this.router(...args));
-        })
+            window.addEventListener('route', () => {
+                // @ts-ignore
+                this.routing(this.constructor.routes, (...args) => this.router(...args));
+            })
 
-        window.onpopstate = () => {
+            window.onpopstate = () => {
+                window.dispatchEvent(new CustomEvent('route'));
+            }
+        }
+
+        routed(name, params, query, data, callback, localCallback) {
+            localCallback && localCallback(name, params, query, data);
+            callback(name, params, query, data);
+        }
+
+        routing(routes, callback) {
+            this.canceled = true;
+
+            const uri = decodeURI(window.location.pathname);
+            const querystring = decodeURI(window.location.search);
+
+            let notFoundRoute = routes.filter(route => route.pattern === '*')[0];
+            let activeRoute = routes.filter(route => route.pattern !== '*' && testRoute(uri, route.pattern))[0];
+            let query = parseQuery(querystring);
+
+            if (activeRoute) {
+                activeRoute.params = parseParams(activeRoute.pattern, uri);
+                activeRoute.data = activeRoute.data || {};
+                if (activeRoute.authentication && activeRoute.authentication.authenticate && typeof activeRoute.authentication.authenticate === 'function') {
+                    this.canceled = false;
+                    Promise.resolve(activeRoute.authentication.authenticate())
+                        .then((authenticated) => {
+                            if (!this.canceled) {
+                                if (authenticated) {
+                                    if (activeRoute.authorization && activeRoute.authorization.authorize && typeof activeRoute.authorization.authorize === 'function') {
+                                        this.canceled = false;
+                                        Promise.resolve(activeRoute.authorization.authorize())
+                                            .then((authorizatied) => {
+                                                if (!this.canceled) {
+                                                    if (authorizatied) {
+                                                        this.routed(activeRoute.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
+                                                    } else {
+                                                        this.routed(activeRoute.authorization.unauthorized.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
+                                                    }
+                                                }
+                                            })
+                                    } else {
+                                        this.routed(activeRoute.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
+                                    }
+                                } else {
+                                    this.routed(activeRoute.authentication.unauthenticated.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
+                                }
+                            }
+                        })
+                } else if (activeRoute.authorization && activeRoute.authorization.authorize && typeof activeRoute.authorization.authorize === 'function') {
+                    this.canceled = false;
+                    Promise.resolve(activeRoute.authorization.authorize())
+                        .then((authorizatied) => {
+                            if (!this.canceled) {
+                                if (authorizatied) {
+                                    this.routed(activeRoute.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
+                                } else {
+                                    this.routed(activeRoute.authorization.unauthorized.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
+                                }
+                            }
+                        })
+                } else {
+                    this.routed(activeRoute.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
+                }
+            } else if (notFoundRoute) {
+                notFoundRoute.data = notFoundRoute.data || {};
+                this.routed(notFoundRoute.name, {}, query, notFoundRoute.data, callback, notFoundRoute.callback);
+            }
+        }
+    };
+}
+
+export function navigator(base) {
+    return class extends base {
+        navigate(href) {
+            window.history.pushState({}, null, href);
             window.dispatchEvent(new CustomEvent('route'));
         }
-    }
+    };
+}
 
-    routed(name, params, query, data, callback, localCallback) {
-        localCallback && localCallback(name, params, query, data);
-        callback(name, params, query, data);
-    }
+export function outlet(base) {
+    return class extends base {
 
-    routing(routes, callback) {
-        this.canceled = true;
+        static get properties() {
+            return {
+                activeRoute: { type: String, reflect: true, attribute: 'active-route' }
+            };
+        }
 
-        const uri = decodeURI(window.location.pathname);
-        const querystring = decodeURI(window.location.search);
+        attributeChangedCallback(...args) {
+            super.attributeChangedCallback(...args);
 
-        let notFoundRoute = routes.filter(route => route.pattern === '*')[0];
-        let activeRoute = routes.filter(route => route.pattern !== '*' && testRoute(uri, route.pattern))[0];
-        let query = parseQuery(querystring);
+            args.some(arg => arg === 'active-route') && this.outlet();
+        }
 
-        if (activeRoute) {
-            activeRoute.params = parseParams(activeRoute.pattern, uri);
-            activeRoute.data = activeRoute.data || {};
-            if (activeRoute.authentication && activeRoute.authentication.authenticate && typeof activeRoute.authentication.authenticate === 'function') {
-                this.canceled = false;
-                Promise.resolve(activeRoute.authentication.authenticate())
-                    .then((authenticated) => {
-                        if (!this.canceled) {
-                            if (authenticated) {
-                                if (activeRoute.authorization && activeRoute.authorization.authorize && typeof activeRoute.authorization.authorize === 'function') {
-                                    this.canceled = false;
-                                    Promise.resolve(activeRoute.authorization.authorize())
-                                        .then((authorizatied) => {
-                                            if (!this.canceled) {
-                                                if (authorizatied) {
-                                                    this.routed(activeRoute.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
-                                                } else {
-                                                    this.routed(activeRoute.authorization.unauthorized.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
-                                                }
-                                            }
-                                        })
-                                } else {
-                                    this.routed(activeRoute.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
-                                }
-                            } else {
-                                this.routed(activeRoute.authentication.unauthenticated.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
-                            }
-                        }
-                    })
-            } else if (activeRoute.authorization && activeRoute.authorization.authorize && typeof activeRoute.authorization.authorize === 'function') {
-                this.canceled = false;
-                Promise.resolve(activeRoute.authorization.authorize())
-                    .then((authorizatied) => {
-                        if (!this.canceled) {
-                            if (authorizatied) {
-                                this.routed(activeRoute.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
-                            } else {
-                                this.routed(activeRoute.authorization.unauthorized.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
-                            }
-                        }
-                    })
-            } else {
-                this.routed(activeRoute.name, activeRoute.params, query, activeRoute.data, callback, activeRoute.callback);
+        connectedCallback(...args) {
+            super.connectedCallback(...args);
+
+            setImmediate(() => {
+                this.outlet();
+            });
+        }
+
+        outlet() {
+            Array.from(this.querySelectorAll(`[route]`)).map((active) => {
+                active.style.display = "none";
+            });
+            Array.from(this.shadowRoot.querySelectorAll(`[route]`)).map((active) => {
+                active.style.display = "none";
+            });
+            if (this.activeRoute) {
+                Array.from(this.querySelectorAll(`[route~=${this.activeRoute}]`)).map((active) => {
+                    active.style.display = "";
+                });
+                Array.from(this.shadowRoot.querySelectorAll(`[route~=${this.activeRoute}]`)).map((active) => {
+                    active.style.display = "";
+                });
             }
-        } else if (notFoundRoute) {
-            notFoundRoute.data = notFoundRoute.data || {};
-            this.routed(notFoundRoute.name, {}, query, notFoundRoute.data, callback, notFoundRoute.callback);
         }
-    }
-};
-
-export let navigateMixin = (superclass) => class extends superclass {
-    navigate(href) {
-        window.history.pushState({}, null, href);
-        window.dispatchEvent(new CustomEvent('route'));
-    }
-};
-
-export let outletMixin = (superclass) => class extends superclass {
-
-    static get properties() {
-        return {
-            activeRoute: { type: String, reflect: true, attribute: 'active-route' }
-        };
-    }
-
-    attributeChangedCallback(...args) {
-        super.attributeChangedCallback(...args);
-
-        args.some(arg => arg === 'active-route') && this.outlet();
-    }
-
-    connectedCallback(...args) {
-        super.connectedCallback(...args);
-
-        setImmediate(() => {
-            this.outlet();
-        });
-    }
-
-    outlet() {
-        Array.from(this.querySelectorAll(`[route]`)).map((active) => {
-            active.style.display = "none";
-        });
-        Array.from(this.shadowRoot.querySelectorAll(`[route]`)).map((active) => {
-            active.style.display = "none";
-        });
-        if (this.activeRoute) {
-            Array.from(this.querySelectorAll(`[route~=${this.activeRoute}]`)).map((active) => {
-                active.style.display = "";
-            });
-            Array.from(this.shadowRoot.querySelectorAll(`[route~=${this.activeRoute}]`)).map((active) => {
-                active.style.display = "";
-            });
-        }
-    }
-};
+    };
+}
