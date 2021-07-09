3c2434e34ef1d20c102349d5e943f74f062a7003
Lucien Immink <lucien@arielext.org>
2021-07-09 11:39:50 +0200

    fix undefined error

--- lit-element-router.js
+++ lit-element-router.js
@@ -209,20 +209,24 @@ export function outlet(base) {
       Array.from(this.querySelectorAll(`[route]`)).map((active) => {
         active.style.display = "none";
       });
-      Array.from(this.shadowRoot?.querySelectorAll(`[route]`)).map((active) => {
-        active.style.display = "none";
-      });
+      if (this.shadowRoot) {
+        Array.from(this.shadowRoot.querySelectorAll(`[route]`)).map((active) => {
+          active.style.display = "none";
+        });
+      }
       if (this.activeRoute) {
         Array.from(this.querySelectorAll(`[route~=${this.activeRoute}]`)).map(
           (active) => {
             active.style.display = "";
           }
         );
-        Array.from(
-          this.shadowRoot?.querySelectorAll(`[route~=${this.activeRoute}]`)
-        ).map((active) => {
-          active.style.display = "";
-        });
+        if (this.shadowRoot) {
+          Array.from(
+            this.shadowRoot.querySelectorAll(`[route~=${this.activeRoute}]`)
+          ).map((active) => {
+            active.style.display = "";
+          });
+        }
       }
     }
   };
